561138e91d0ad9e8b8d942f807a98b4d126e517c
Yuri Burger <yburger@vxcompany.com>
2020-11-12 03:13:59 +0100

    .net core en spring boot toegevoegd

---
+++ training/hello-from-dotnetcore/README.md
@@ -0,0 +1,1 @@
+# Demo app voor .Net Core
--- training/hello-from-nodejs/server.js
+++ training/hello-from-nodejs/server.js
@@ -8,7 +8,7 @@ const HOST = '0.0.0.0';
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello from Node.js');
 });
 
 app.listen(PORT, HOST);
